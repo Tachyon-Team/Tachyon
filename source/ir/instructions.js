3273f35507d670d66d674c4f8833507d36519293
Maxime <maxime@maxime-laptop.(none)>
2010-06-06 17:41:00 -0400

    - Committed new files - ir subfolder to contain ir manipulation code - lowlevel subfolder to contain low level (unsafe) interfaces

---
+++ source/ir/instructions.js
@@ -0,0 +1,10 @@
+// Base class for all instructions
+function BaseInstr()
+{
+    this.toString = function () {}
+}
+
+// Class for arithmetic instructions
+function ArithInstr()
+{
+}
---
+++ source/lowlevel/memory.js
@@ -0,0 +1,14 @@
+
+//
+// This file will include wrapper code for low-level, raw memory access, which is
+// necessary for bootstrapping.
+//
+//
+
+function write_u8(ptr, val)
+{
+}
+
+function read_u8(ptr, val)
+{
+}
