cbf70895b485db15bc67cb7b8c96a7ee315b5a14
Maxime <maxime@maxime-laptop.(none)>
2010-06-07 00:03:03 -0400

    - Renamed functions in memory.js to start with "mem" prefix - Added pointer manipulation functions

--- source/lowlevel/memory.js
+++ source/lowlevel/memory.js
@@ -1,7 +1,10 @@
 /**
 @fileOverview
 This file includes wrapper code for low-level, raw memory access, which is
-necessary for bootstrapping.
+necessary for bootstrapping. When running under D8, these functions will be
+calling external C/C++ host functions to do their work. In fully bootstrapped
+mode, however, the VM will recognize calls to these functions and replace them
+by native instructions directly.
 
 @author
 Maxime Chevalier-Boisvert
@@ -11,11 +14,31 @@ Copyright (c) 2010 Maxime Chevalier-Boisvert, All Rights Reserved
 */
 
 /**
+@description Adds a signed offset to a pointer value
+@param ptr pointer to raw memory
+@param val signed offset
+@returns the adjusted pointer
+*/
+function memPtrAdd(ptr, offset)
+{
+}
+
+/**
+@description Computes the difference between two pointers
+@param ptr1 first pointer
+@param ptr2 second pointer
+@returns the integer pointer difference
+*/
+function memPtrSub(ptr1, ptr2)
+{
+}
+
+/**
 @description Writes an 8-bit unsigned integer to memory
 @param ptr pointer to raw memory
 @param val value to write
 */
-function unsafeWriteU8(ptr, val)
+function memWriteU8(ptr, val)
 {
 }
 
@@ -24,7 +47,7 @@ function unsafeWriteU8(ptr, val)
 @param ptr pointer to raw memory
 @returns value read
 */
-function unsafeReadU8(ptr)
+function memReadU8(ptr)
 {
 }
 
