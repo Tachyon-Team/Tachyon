a89b610511f074dd68e9914e4bb7790cc008c4cf
Erick Lavoie <erick.lavoie@gmail.com>
2010-08-16 19:43:37 -0400

    Added an empty line to testmain.js header

--- source/tests/testmain.js
+++ source/tests/testmain.js
@@ -5,6 +5,7 @@ Entry point for the testing mode.
 @copyright
 Copyright (c) 2010 Tachyon Javascript Engine, All Rights Reserved
 */
+
 // Flag to enable or disable unit tests
 // NOTE: will be useful to enable unit testing through the command-line
 var testsEnabled = true;
