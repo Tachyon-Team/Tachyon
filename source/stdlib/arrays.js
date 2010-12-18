a06c72d855e03b04afb08614fc85237980b09194
maximecb <maximechevalierb@gmail.com>
2010-12-18 17:42:01 -0500

    - Renamed source/jslib directory to source/stdlib - Added stdlib/math.js, stdlib/arrays.js, stdlib/strings.js
      I suggest noting needed library functions for the initial bootstrap
      in comments in these files. These can then be implemented progressively
      as we get sufficient functionality.

---
+++ source/stdlib/arrays.js
@@ -0,0 +1,21 @@
+/**
+@fileOverview
+Implementation of ECMAScript 5 array library routines.
+
+@author
+TBD
+
+@copyright
+Copyright (c) 2010 Tachyon Javascript Engine, All Rights Reserved
+*/
+
+/*
+TODO: establish functionality needed for initial bootstrap
+
+Array.prototype.map
+Array.prototype.forEach
+...
+
+*/
+
+
---
+++ source/stdlib/strings.js
@@ -0,0 +1,22 @@
+/**
+@fileOverview
+Implementation of ECMAScript 5 array string routines.
+
+@author
+TBD
+
+@copyright
+Copyright (c) 2010 Tachyon Javascript Engine, All Rights Reserved
+*/
+
+/*
+TODO: establish functionality needed for initial bootstrap
+
+String.prototype.charAt
+String.prototype.charCodeAt
+String.prototype.substring
+...
+
+*/
+
+
