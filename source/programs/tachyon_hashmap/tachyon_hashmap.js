2c0419b9d23aa48aa04215e3a59bab365cc64c9d
Maxime <maxime@maxime-laptop.(none)>
2011-02-21 11:49:43 -0500

    - Added more tests for hash map, all working

--- source/programs/tachyon_hashmap/tachyon_hashmap.js
+++ source/programs/tachyon_hashmap/tachyon_hashmap.js
@@ -2,16 +2,26 @@ function test()
 {
     var map = new HashMap();
 
-    var keyList = ['a', 'b', 'c', 1, 2, 3];
-    var valList = [1, 2, 3, 'a', 'b', 'c'];
+    var keyList = [];
+    var valList = [];
 
-    /*
+    for (var i = 0; i < 50; ++i)
+    {
+        keyList.push('k' + i);
+        valList.push(i);
+    }
+
+    for (var i = 0; i < 50; ++i)
+    {
+        keyList.push(i);
+        valList.push('v' + i);
+    }
+
     print('num items: ' + map.numItems);
     print('num slots: ' + map.numSlots);
     print('array length: ' + map.array.length);
 
     print('adding items');
-    */
 
     for (var i = 0; i < keyList.length; ++i)
     {
@@ -30,24 +40,63 @@ function test()
 
     for (var i = 0; i < keyList.length; ++i)
     {
+        if (!map.hasItem(keyList[i]))
+            return 1;
+
         var val = map.getItem(keyList[i]);
 
         /*
         print('key: ' + keyList[i]);
         print('val: ' + valList[i]);
-        print('got val: ' + val);
+        print('got: ' + val);
         */
 
         if (val !== valList[i])
-            return 1;
+            return 2;
     }
 
-    //
-    // TODO: **** add more tests here ****
-    // - hasItem
-    // - remItem
-    // - iterator
-    //
+    ITR_LOOP:
+    for (var itr = map.getItr(); itr.valid(); itr.next())
+    {
+        var cur = itr.get();
+
+        for (var i = 0; i < keyList.length; ++i)
+        {
+            if (keyList[i] === cur.key)
+            {
+                if (valList[i] !== cur.value)
+                    return 3;
+
+                continue ITR_LOOP;
+            }
+        }
+
+        return 4;
+    }
+
+    for (var i = 0, c = 0; i < keyList.length; ++i, ++c)
+    {
+        if (c % 3 === 0)
+        {
+            map.remItem(keyList[i]);
+
+            keyList.splice(i, 1);
+            valList.splice(i, 1);
+
+            --i;
+        }
+    }
+
+    for (var i = 0; i < keyList.length; ++i)
+    {
+        if (!map.hasItem(keyList[i]))
+            return 5;
+
+        var val = map.getItem(keyList[i]);
+
+        if (val !== valList[i])
+            return 6;
+    }
 
     return 0;
 }
