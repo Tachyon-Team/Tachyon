06a635164984949fb37677e5fdc2ee61c569bb6d
Erick Lavoie <erick.lavoie@gmail.com>
2011-03-05 13:42:50 -0500

    Memory usage script: allow specifying a file through command line args

--- source/mem.py
+++ source/mem.py
@@ -1,11 +1,20 @@
-lines = open("gc_trace.txt").readlines()
+import sys
 
-used = 0
-for line in lines:
-    if line[:5] == "pause":
-        l = [token.split("=") for token in line.split(" ")[:-1]]
+def main(file):
+    lines = open(file).readlines()
 
-        d = dict(l)
-        used += int(d["allocated"])
+    used = 0
+    for line in lines:
+        if line[:5] == "pause":
+            l = [token.split("=") for token in line.split(" ")[:-1]]
+            d = dict(l)
+            used += int(d["allocated"])
 
-print "Memory used: %f MB"%(used/1000000.0)
+    print "Memory used: %f MB"%(used/1000000.0)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+    else:
+        file = "gc_trace.txt"
+    main(file)
