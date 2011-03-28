a9c014e65690039eb00b6ea87a995adda7d46ee4
Maxime <maxime@maxime-laptop.(none)>
2011-03-27 23:22:38 -0400

    - Changed code opt example - Removed redundant slides

--- talks/mozilla-mar11/images/sum.js
+++ talks/mozilla-mar11/images/sum.js
@@ -1,9 +1,15 @@
+var zero = 0;
+
 function sum(list) {
-    var sum = 0;
+    var sum = zero;
     for (var i = 0; i < list.length; ++i)
-        sum += f(list[i]);
+    {
+        var t = list[i];
+        sum = sum + t;
+    }
     return sum;
 }
 
-function f(v) { return v*v; }
+function f(x) { zero = x; }
+
 print(sum([1,2,3,4,5]));
