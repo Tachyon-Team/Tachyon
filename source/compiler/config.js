460b0823d291b3369a2cbbd17dd3c304167eae8a
Erick Lavoie <erick.lavoie@gmail.com>
2011-05-11 16:47:31 -0400

    RegAlloc: Change default allocator for onthefly

--- source/compiler/config.js
+++ source/compiler/config.js
@@ -105,7 +105,6 @@ function initConfig(is64bit, verbosity)
     config.clientParams = Object.create(config.hostParams);
     config.clientParams.tachyonSrc = false;
     config.clientParams.parserWarnings = false;
-    config.clientParams.regAlloc = "onthefly";
 
     /**
     Compilation parameters for debugging client code.
